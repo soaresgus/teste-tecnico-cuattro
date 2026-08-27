2075780ed7d33cbb61ea4e2fbff47a7200f0179f
soaresgus <gustavosoaresbt@hotmail.com>
2026-08-27 17:15:30 -0300

    build: correction on web utils import to approve CI

--- web/src/components/ui/dialog.tsx
+++ web/src/components/ui/dialog.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { Dialog as DialogPrimitive } from "@base-ui/react/dialog"
 import { XIcon } from "lucide-react"
 
-import { cn } from "../../lib/utils"
+import { cn } from "../../lib/utils.ts"
 import { Button } from "./button"
 
 function Dialog({ ...props }: DialogPrimitive.Root.Props) {
